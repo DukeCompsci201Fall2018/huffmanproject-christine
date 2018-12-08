d0f8369a9c952ff0815ad334f21631f0819734ad
Christine Yoon <cy125@duke.edu>
2018-12-07 23:10:16 -0500

    update

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -60,15 +60,14 @@ public class HuffProcessor {
 	 */
 	public void decompress(BitInputStream in, BitOutputStream out){
 
-//		while (true){
-//			int val = in.readBits(BITS_PER_WORD);
-//			if (val == -1) break;
-//			out.writeBits(BITS_PER_WORD, val);
-//		}
 		int bits = in.readBits(BITS_PER_INT);
-//		if (bits != HUFF_TREE) {
-		if (bits == -1) {
+		if (bits != HUFF_TREE) { 
 			throw new HuffException("illegal header starts with " +bits);
+		}
+		while (true){
+		int val = in.readBits(BITS_PER_WORD);
+		if (val == -1) break;
+		out.writeBits(BITS_PER_WORD, val);
 		}
 		HuffNode root = readTreeHeader(in);
 		readCompressedBits(root, in, out);
